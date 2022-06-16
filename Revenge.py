88478b2d5eebaa47c104b5def01a979b9a62490a
Biruk-Tassew <biruktassew21@gmail.com>
2022-06-16 09:52:32 -0400

    fixes on the logic

--- Revenge.py
+++ Revenge.py
@@ -32,6 +32,30 @@ log2_rand_pos = -400
 score = 0
 counter_keep = 3
 
+def retry_window(score_display, message, btn):
+    global log_rand_pos, log2_rand_pos, score, counter_keep
+    window = Tk()
+    window.wm_attributes('-type', 'splash')
+    window.protocol('WM_DELETE_WINDOW', lambda: None) 
+    window.geometry("300x150+550+300")
+    window.configure(bg=("#%02x%02x%02x" % (19, 24, 98)))
+    temp = tk.Frame(window)
+    temp.pack(side='bottom')
+    text = Label(temp, text=message)
+    text.pack()
+    score_dis = Label(temp, text=("Score: " + str(score_display)))
+    score_dis.pack()    
+    btn_retry = tk.Button(temp, text =btn, command = window.destroy)
+    
+    btn_retry.pack(side='left')
+    btn_cancel = tk.Button(temp, text ="Quit", command = lambda: sys.exit())
+    btn_cancel.pack(side='left')
+    window.call('wm', 'attributes', '.', '-topmost', '1')
+    log_rand_pos = -70.0
+    log2_rand_pos = -70.0
+    score = -2
+    counter_keep = 3
+    window.mainloop()
 
    
 def revenge():
@@ -41,8 +65,27 @@ def revenge():
 
     # loading the objects
 
-    #this is for the man
+    #this is for the man....20 frames
     man_obj_frame1 = OBJ('assets/man/man_000001.obj', swapyz=True)
+    man_obj_frame2 = OBJ('assets/man/man_000002.obj', swapyz=True)
+    man_obj_frame3 = OBJ('assets/man/man_000003.obj', swapyz=True)
+    man_obj_frame4 = OBJ('assets/man/man_000004.obj', swapyz=True)
+    man_obj_frame5 = OBJ('assets/man/man_000005.obj', swapyz=True)
+    man_obj_frame6 = OBJ('assets/man/man_000006.obj', swapyz=True)
+    man_obj_frame7 = OBJ('assets/man/man_000007.obj', swapyz=True)
+    man_obj_frame8 = OBJ('assets/man/man_000008.obj', swapyz=True)
+    man_obj_frame9 = OBJ('assets/man/man_000009.obj', swapyz=True)
+    man_obj_frame10 = OBJ('assets/man/man_000010.obj', swapyz=True)
+    man_obj_frame11 = OBJ('assets/man/man_000011.obj', swapyz=True)
+    man_obj_frame12 = OBJ('assets/man/man_000012.obj', swapyz=True)
+    man_obj_frame13 = OBJ('assets/man/man_000013.obj', swapyz=True)
+    man_obj_frame14 = OBJ('assets/man/man_000014.obj', swapyz=True)
+    man_obj_frame15 = OBJ('assets/man/man_000015.obj', swapyz=True)
+    man_obj_frame16 = OBJ('assets/man/man_000016.obj', swapyz=True)
+    man_obj_frame17 = OBJ('assets/man/man_000017.obj', swapyz=True)
+    man_obj_frame18 = OBJ('assets/man/man_000018.obj', swapyz=True)
+    man_obj_frame19 = OBJ('assets/man/man_000019.obj', swapyz=True)
+    man_obj_frame20 = OBJ('assets/man/man_000020.obj', swapyz=True)
 
     # loading the road model
     road_model = OBJ('assets/road/road.obj', swapyz=True)
@@ -64,7 +107,25 @@ def revenge():
 
     # generating the objects
     man_obj_frame1.generate()
-   
+    man_obj_frame2.generate()
+    man_obj_frame3.generate()
+    man_obj_frame4.generate()
+    man_obj_frame5.generate()
+    man_obj_frame6.generate()
+    man_obj_frame7.generate()
+    man_obj_frame8.generate()
+    man_obj_frame9.generate()
+    man_obj_frame10.generate()
+    man_obj_frame11.generate()
+    man_obj_frame12.generate()
+    man_obj_frame13.generate()
+    man_obj_frame14.generate()
+    man_obj_frame15.generate()
+    man_obj_frame16.generate()
+    man_obj_frame17.generate()
+    man_obj_frame18.generate()
+    man_obj_frame19.generate()
+    man_obj_frame20.generate()
     road_model.generate()
     dinasour_model.generate()
     building_model.generate()
@@ -90,7 +151,10 @@ def revenge():
     time_comp = 0
     
 
-    
+    man_frame_list = [man_obj_frame1, man_obj_frame2, man_obj_frame3, man_obj_frame4, man_obj_frame5, man_obj_frame6, man_obj_frame7, man_obj_frame8,
+                     man_obj_frame9, man_obj_frame10, man_obj_frame11, man_obj_frame12, man_obj_frame13, man_obj_frame14, man_obj_frame15,
+                     man_obj_frame16, man_obj_frame17, man_obj_frame18, man_obj_frame19, man_obj_frame20
+    ]
     left_bld_pos = [-680, 580.0, -360.0, -230.0, -100.0]
     comp_left_bld_pos = [-820, 720.0, -660.0, -620.0, -500.0]
     right_bld_pos = [-600, 520.0, -360.0, -230.0, -100.0]
@@ -223,7 +287,7 @@ def revenge():
         #Rendering the man
         glTranslate(0.0, man_ver_pos, -40.0) 
         glRotate(90.0, -1.0, 0.0, 0.0) 
-        man_frame_obj1.render()
+        man_frame_list[man_frame_index].render()
         glRotate(90.0, 1.0, 0.0, 0.0)  
         glTranslate(0.0, -1*man_ver_pos, 40.0)  
 
@@ -248,8 +312,21 @@ def revenge():
         pygame.display.flip()
         pygame.time.wait(20)
 
-       
+        # this code makes the counting when the player start playing or retry
+        if counter_keep:
+            font = pygame.font.SysFont('arial', 64)
+            text_surface = font.render(str(counter_keep), True, (0,255,255,255), (0, 19, 24, 98))
+            text_data = pygame.image.tostring(text_surface, "RGBA", True)
+            glWindowPos2d(350, 350)
+            glDrawPixels(text_surface.get_width(), text_surface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, text_data)
+            pygame.display.flip()
+            time.sleep(1)
+            counter_keep -= 1
 
+        # if the position of the logs and the man is equal, the it's game over
+        # call the retry_window functio to display a tkinter window asking if the user wants to play again or quit
+        if (log_rand_pos == -100.0 or log2_rand_pos == -100) and man_ver_pos == -7.0:
+            retry_window(score, "GAME OVER!!!", "Retry")
         
         # when the logs reach to the dinosaur's position we remove them from the screen
         if log_rand_pos >= -80:
@@ -266,7 +343,12 @@ def revenge():
         if dina_pos < -5:
                 dina_pos += 0.05
 
+        # changing the frames for the man
+        if man_frame_index>= 19:
+            man_frame_index= 1
         
+        man_frame_index+= 1
+        time_comp += 1
 
         # while the position of the buildings is out of the viewport, we need to bring them back to their first position
         if left_bld_pos[0] >= -60:
